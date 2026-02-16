695d00af9e673e0e99070048407e9fa98abd00fe
lucasmartini-dev <lucassoftwares@gmail.com>
2026-02-16 23:47:12 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS sum;");
-  console.log(result.rows);
-  response.status(200).json({ key: "they are above average" });
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const bdVersionValue = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConnectionsValue = parseInt(
+    dbMaxConnectionsResult.rows[0].max_connections,
+  );
+
+  const dbOpenedConnectionsResult = await database.query(
+    "SELECT count(*) FROM pg_stat_activity WHERE datname = current_database();",
+  );
+  const dbOpenedConnectionsValue = parseInt(
+    dbOpenedConnectionsResult.rows[0].count,
+  );
+
+  response.status(200).json({
+    update_at: new Date().toISOString(),
+    dependencies: {
+      database: {
+        version: bdVersionValue,
+        max_connections: dbMaxConnectionsValue,
+        opened_connections: dbOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
