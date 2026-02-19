f69678638322a199eaeb1f5b1904ebfe33682250
lucasmartini-dev <lucassoftwares@gmail.com>
2026-02-19 12:59:06 +0000

    add `/api/v1/migrations` endpoint

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const nextJest = require("next/jest");
+const createJestConfig = nextJest({
+  dir: ".",
+});
+
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+});
+
+const dotenv = require("dotenv");
+dotenv.config({
+  path: ".env.development",
+});
+
+module.exports = jestConfig;
---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,25 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
+}
+
+test("POST to /api/v1/migrations should return 201 and 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response.status).toBe(201);
+  const responseBody = await response.json();
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+
+  const responseCheck = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(responseCheck.status).toBe(200);
+  const responseCheckBody = await responseCheck.json();
+  expect(Array.isArray(responseCheckBody)).toBe(true);
+  expect(responseCheckBody.length).toBe(0);
+});
