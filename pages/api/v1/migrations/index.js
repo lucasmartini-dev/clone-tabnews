651b822145ad4e2d2afe04607777ed394f00a423
lucasmartini-dev <lucassoftwares@gmail.com>
2026-02-20 01:19:07 +0000

    improve database connections from `/migrations` endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,35 +3,44 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    await dbClient.end();
-    return response.status(200).json(pendingMigrations);
+  if (request.method !== "GET" && request.method !== "POST") {
+    return response.status(405).json({});
   }
-  if (request.method === "POST") {
-    const doneMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
-    await dbClient.end();
 
-    if (Array.isArray(doneMigrations) && doneMigrations.length > 0) {
-      return response.status(201).json(doneMigrations);
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+    const defaultMigrationOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      await dbClient.end();
+      return response.status(200).json(pendingMigrations);
     }
-    return response.status(200).json(doneMigrations);
-  }
+    if (request.method === "POST") {
+      const doneMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+      await dbClient.end();
 
-  await dbClient.end();
-  return response.status(405).json({});
+      if (Array.isArray(doneMigrations) && doneMigrations.length > 0) {
+        return response.status(201).json(doneMigrations);
+      }
+      return response.status(200).json(doneMigrations);
+    }
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error handling migrations.");
+  } finally {
+    await dbClient.end();
+  }
 }
 
 export default migrations;
