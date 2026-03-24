497e014f2b3cc25a2ceaf4739b0ad03d2b643621
lucasmartini-dev <lucassoftwares@gmail.com>
2026-03-24 23:56:23 +0000

    feat: add `/status` page with `database` info

---
+++ pages/status/index.js
@@ -0,0 +1,57 @@
+import useSWR from "swr";
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  return responseBody;
+}
+
+export default function StatusPage() {
+  return <StatusCompoment />;
+}
+
+function StatusCompoment() {
+  let updatedAtText = "Loading...";
+  let database;
+
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 1999,
+  });
+
+  if (!isLoading && data?.update_at) {
+    updatedAtText = new Date(data.update_at).toLocaleString("pt-BR");
+    if (data?.dependencies?.database) {
+      database = data.dependencies.database;
+    }
+  }
+
+  return (
+    <>
+      <h1>Status</h1>
+      <h2>Last update: {updatedAtText}</h2>
+      <br></br>
+      <h1>Database</h1>
+      <ul>
+        <li>
+          <h2>
+            Version: {isLoading && !database ? "Loading..." : database.version}
+          </h2>
+        </li>
+        <li>
+          <h2>
+            Max Connections:{" "}
+            {isLoading && !database ? "Loading..." : database.max_connections}
+          </h2>
+        </li>
+        <li>
+          <h2>
+            Opened Connections:{" "}
+            {isLoading && !database
+              ? "Loading..."
+              : database.opened_connections}
+          </h2>
+        </li>
+      </ul>
+    </>
+  );
+}
